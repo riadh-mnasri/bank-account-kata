5c2ec7d5e064c6970a4fd5caec99207139450956
Riadh MNASRI <riadh.mnasri@gmail.com>
2020-10-28 00:29:34 +0100

    feat(bank-account-kata) Add coding base

---
+++ src/test/kotlin/com/riadhmnasri/bankaccountkata/HelloTest.java
@@ -0,0 +1,11 @@
+package com.riadhmnasri.bankaccountkata;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class HelloTest {
+    @Test
+    void test_hello() {
+        Assertions.assertThat(true);
+    }
+}
